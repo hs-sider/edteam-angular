58a85f6cdac4a78ff38b09e6c835f88aa962d989
Sider Huanca <sider.huanca@jalasoft.com>
2018-08-29 21:52:48 -0400

    Clase-03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'material-demo', // http://localhost:4200/material-demo => Material-demo works!
+    component: MaterialDemoComponent
+    // children: []
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
